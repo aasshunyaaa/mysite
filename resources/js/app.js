d17c1522edcc44960a6921673dfc0b6bdf2f583d
akai1 <akai@shiftplus.jp>
2021-09-27 00:34:00 +0900

    aaa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+require('./bootstrap');
+
+window.Vue = require('vue');
+// 追加
+Vue.component('sample-component', require('./components/SampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
